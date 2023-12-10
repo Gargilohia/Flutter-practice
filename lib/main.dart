bb2eeed79f9ede504c713d436b189e4104793c78
Gargilohia <gargilohia2001@yahoo.com>
2023-12-10 23:00:21 +0530

    added linear gradient

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,20 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.deepPurple,
-        body: Center(
+        // ignore: avoid_unnecessary_containers
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Color.fromARGB(137, 241, 29, 29), 
+               Color.fromARGB(66, 243, 157, 45)],
+            ),
+          ),
+        child: const Center(
           child: Text('Namaste!'),
         ),
+      ),
       ),
     ),
   );
