10dd503f6cd851c89af69a60a387dc439219657b
Gargilohia <gargilohia2001@yahoo.com>
2024-01-31 14:30:16 +0900

    made constant variables for alignment

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:test_app/styled_text.dart';
 
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
 class GradientContainer extends StatelessWidget{
 
   const GradientContainer({super.key});
@@ -11,8 +14,8 @@ class GradientContainer extends StatelessWidget{
             gradient: LinearGradient(
               colors: [Color.fromARGB(137, 241, 29, 29), 
                Color.fromARGB(66, 243, 157, 45)],
-               begin: Alignment.topLeft,
-               end: Alignment.bottomRight,
+               begin: startAlignment,
+               end: endAlignment,
             ),
           ),
         child: const Center(
